a3dbcbba7a91931b83c90b03871b798867b70562
不做白日梦 <1819222424@qq.com>
2020-09-09 16:46:57 +0800

    修改了hello文件

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -6,5 +6,6 @@ public class hello {
         public static void main(String[] args) {
                 System.out.println("");
                 System.out.println("hi");
+                System.out.println("您好");
         }
 }
